ce85a926153ec1cc66c9cbb508bac9458a57b5a7
jleightcap <jack@leightcap.com>
2022-11-05 02:22:53 -0400

    TEST(jl): pin nix version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation {
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4f09cfce9c1d54fb56b65125061a632849de1a49.tar.gz") {}
+}:
+with pkgs; stdenv.mkDerivation {
   name = "j4";
-
 
   nativeBuildInputs = [
     verilog
